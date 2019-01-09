6399b928053089e536ccd6024910d2479c40c291
sashajustice <sabrinnasreddin@gmail.com>
2019-01-08 20:22:42 -0800

    Add jpg links

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import ContactCard from './ContactCard'
 
-function App(props) {
+function App() {
     return (
-      <div className="contacts">
-        <ContactCard
-          contact={{name: "David Ryan, imgUrl: "", phone: "(510) 222-1234", email: "davidryan@gmail.com}}
-      />
-
-      <ContactCard
-         contact={{name: "Yuri Navastriz, imgUrl: "", phone: "(646) 651-1298", email: "YuriNavastriz@gmail.com}}
-      />
-
-
-     <ContactCard
-     contact={{name: "Michael Androz, imgUrl: "", phone: "(510) 254-1255", email: "MichaelAndroz@gmail.com}}
-      />
+   <div className="contacts">
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "https://i.cbc.ca/1.4046240.1490814905!/fileImage/httpImage/image.png_gen/derivatives/16x9_780/acqua-de-gio.png", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "https://static1.squarespace.com/static/59dbeeaff6576e8869f3fbbc/t/5b2abcac575d1f14a07ac0c1/1529527474104/Willy+Cartier.jpg", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://3.bp.blogspot.com/-u8CRSri9rTg/T5udQKiU2dI/AAAAAAAAFVk/2UVC3C8sVJw/s1600/hot-male-models-04.jpg", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "https://i.pinimg.com/originals/b8/cb/b2/b8cbb29831425105891c3424980fc085.jpg", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+            
+        </div>
 
 
     )
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 function ContactCard(props) {
   return (
-      <div className="contact-cards">
-      <img src={props.imgUrl}/>
-      <h3>{props.name}</h3>
-      <p>Phone: {props.phone}</p>
-      <p>Email: {props.email}</p>
+    <div className="contact-card">
+      <img src={props.contact.imgUrl}/>
+      <h3>{props.contact.name}</h3>
+      <p>Phone: {props.contact.phone}</p>
+      <p>Email: {props.contact.email}</p>
+    </div>
 
     )
 }
