31f5e0d410ac7fda711966d6c2b5db8e0b38ba9c
sashajustice <sabrinnasreddin@gmail.com>
2019-01-19 22:27:04 -0800

    language cmponent

---
+++ languageDropdown.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+function languageDropdown() {
+  return (
+
+    <div className="languageDrop">
+    <form>
+    <input name="language" type="text">
+      <select name="languages">
+        <option value="english">English</option>
+        <option value="spanish">Spanish</option>
+        <option value="swahili">Swahili</option>
+        <option value="amhari">Amharic</option>
+        <option value="yoruba">Yoruba</option>
+        <option value="ibo"></Ibo</option>
+        <option value="wolof">Wolof</option>
+        </select>
+      </form>
+
+
+<option value="volvo">Volvo</option>
+
+      
+
+    </div>
+
+    )
+}
+
+
+
+
+
+export default languageDropdown
---
+++ list.js
@@ -0,0 +1,27 @@
+
+const contactData = [
+ { 
+  id: 1,
+  name: "Roberto",
+  img:
+  number: "333-2134",
+  email: "roberto@gmail.com"
+},
+
+{
+  no:2,
+  name:"GianLuca",
+  img:,
+  number:"443-5433",
+  email:"GianLuca@gmail.com"
+},
+
+{
+  no:3,
+  name:"massimo",
+  img:,
+  number:576-4738,
+  email:"massimo@gmail.com"
+
+}
+]
